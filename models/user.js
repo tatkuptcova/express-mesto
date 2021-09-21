8589ab17efbfda3eaf060cc48136dfcacc7cf3dc
tatkuptcova <tatkuptcova@yandex.ru>
2021-09-21 22:34:34 +0200

    email, password , default name-about-avatar

--- models/user.js
+++ models/user.js
@@ -1,21 +1,29 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
     required: true,
+    default: 'Жак-Ив Кусто'
   },
+
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
     required: true,
+    default: 'Исследователь'
   },
+
   avatar: {
     type: String,
     required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(v) {
         return /^(http:\/\/|https:\/\/)w*\w/.test(v);
@@ -23,9 +31,45 @@ const userSchema = new mongoose.Schema({
       message: 'Ссылка в некорректном формате',
     },
   },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Пожалуйста, введите правильный email',
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
 },
 {
   versionKey: false,
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  // попытаемся найти пользователя по почте
+  return this.findOne({ email }).select('+password') // this — это модель User
+    .then((user) => {
+      // не нашёлся — отклоняем промис
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
