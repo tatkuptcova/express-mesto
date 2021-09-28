543f157fd5cd59e85f48c1b1eb0178181027f1e1
tatkuptcova <tatkuptcova@yandex.ru>
2021-09-28 21:39:44 +0200

    user.js стрелочная функция заменена на обычную функцию

--- models/user.js
+++ models/user.js
@@ -48,8 +48,8 @@ const userSchema = new mongoose.Schema({
   versionKey: false,
 });
 
-userSchema.statics.findUserByCredentials = (email, password) => {
-  return this.findOne({ email })
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password') 
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
